449892d0a53182a70989231d05b261170ece27fc
jordandivyansh <divyanshkrit@gmail.com>
2020-05-13 02:43:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+ function refresh(){
+                var textContent = document.getElementById('editor-textarea').value;
+                document.getElementById('viewer').srcdoc = textContent;
+            }
+
+            onkeyup="refresh()";
